da53d4daeaec7ac761a323484b333831d72746ee
Adrien Coulier <adrien.coulier@it.uu.se>
2016-09-23 17:13:15 +0200

    Introduce reverse free map generation

--- San_half.cpp
+++ San_half.cpp
@@ -4,24 +4,27 @@
 
 using namespace std;
 
-#include "rev_numbers_sort.txt"
 #include "San.cpp"
 
 int main(){
     map<long long, long long> dp;
-    dp[1] = 1;
-    for(auto i: rev_numbers){
-        dp[i] = 1;
-        vector<long long> gen = reverse(i, 0);
-        for(auto j: gen){
-            if(dp.find(j)!=dp.end()){
-                dp[i] += dp[j];
-            }
-            else{
-                dp[i] += 1;
-            }
+    long long add;
+    long long N = 100;
+    for(auto i=1;i<N;i++){
+        long long new_i = i + rev(i);
+        if(new_i>N)
+            continue;
+        if(dp.find(i)==dp.end())
+            add=1;
+        else{
+            dp[i]+=1;
+            add = dp[i];
         }
 
+        if(dp.find(new_i)==dp.end())
+            dp[new_i] = add;
+        else
+            dp[new_i] += add;
     }
     long long prev = 0;
     cout << "dp[" << 0 << "]=" << 0 << ";\n";
@@ -29,7 +32,7 @@ int main(){
         if (it->first == 1)
             continue;
 
-        cout << "dp[" << it->first << "]=" << it->second + prev -1 << ";\n";
+        cout << "dp[" << it->first << "]=" << it->second + prev - 1 << ";\n";
         prev += it->second-1;
     }
     return 0;
