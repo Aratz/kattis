c466044c4ec99a1ab54e3556cf728c13b8977713
Adrien Coulier <adrien.coulier@it.uu.se>
2016-11-09 21:39:52 +0100

    Add comments

--- Outing.cpp
+++ Outing.cpp
@@ -65,12 +65,13 @@ int main(){
 
     for(int i=0; i<n; i++){
         int j = i;
+        //Traverse graph until a loop is found
         while(mark[j] == -1){
             mark[j] = i;
             j = nodes[j];
         }
         if(mark[j] == i){ //Unvisited cycle detected
-            int k = j;
+            int k = j; //Remember cycle entry node
             int cycle_size = 1;
             while(nodes[j] != k){
                 int jt = j; //Turn cycle into a tree for later union find
@@ -82,6 +83,7 @@ int main(){
         }
     }
 
+    //Accumulate number of children on the roots
     for(int i=0; i<n; i++){
         int root = find_root(i, nodes);
         if(root == i)
@@ -89,6 +91,10 @@ int main(){
         cycles[root].second += 1;
     }
 
+    for(auto v : cycles)
+        cerr << v.first+1 << " " << v.second.first << " " << v.second.second << "\n";
+
+    cerr << fill_dp(roots.size()-1, k) << "\n";
     printf("%d\n", min(k, fill_dp(roots.size()-1, k)));
     return 0;
 }
