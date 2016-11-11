d7254068ce5eff62e7f903a8f29ecdca9749d851
Adrien Coulier <adrien.coulier@it.uu.se>
2016-11-11 23:15:50 +0100

    Nailed Setnja
    BOOYA! AC \o/
    Fastest time to date :D

--- Setnja.py
+++ Setnja.py
@@ -1,22 +1,10 @@
-def compute_string(val, s):
-    for c in s:
-        if c == 'L':
-            val = 2*val
-        elif c == 'R':
-            val = 2*val + 1
-    return val
-
-def compute_star(val, s):
-    sp = s.partition('*')
-    val = compute_string(val, sp[0])
-    if sp[1] == '*':
-        l = compute_star(2*val, sp[2])
-        r = compute_star(2*val + 1, sp[2])
-        p = compute_star(val, sp[2])
-        return l + r + p
-    else:
-        return val
-
 s = input()
-s = s.replace('P', '')
-print(compute_star(1, s))
+val = (1, 1)
+for c in s:
+    if c == 'L':
+        val = (2*val[0], val[1])
+    elif c == 'R':
+        val = (2*val[0] + val[1], val[1])
+    elif c == '*':
+        val = (5*val[0] + val[1], 3*val[1])
+print(val[0])
