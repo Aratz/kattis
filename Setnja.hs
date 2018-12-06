30ed842b70c0177fd4fff6217346448c6b54f932
Adrien Coulier <adrien.coulier@it.uu.se>
2018-12-06 18:27:05 +0100

    Solve Setnja with Haskell

--- Setnja.hs
+++ Setnja.hs
@@ -1,20 +1,13 @@
 import Data.List
 
-computeString :: ([Char], Integer) -> Integer
+computeString :: ([Char], (Integer, Integer)) -> (Integer, Integer)
 computeString ("", val) = val
-computeString (c:s, val)
-    | c=='L' = computeString(s, 2*val)
-    | c=='R' = computeString(s, 2*val + 1)
-    | otherwise = val
-
-computeStar :: ([Char], Integer) -> Integer
-computeStar ("", val) = val
-computeStar (s, val)
-    | not (null a) = computeStar (tail a, 2*newVal) + computeStar (tail a, 2*newVal + 1) + computeStar (tail a, newVal)
-    | otherwise = newVal
-    where (b, a) = span(/='*') s
-          newVal = computeString (b, val)
+computeString (c:s, (val0, val1))
+    | c=='L' = computeString(s, (2*val0, val1 ))
+    | c=='R' = computeString(s, (2*val0 + val1, val1))
+    | c=='*' = computeString(s, (5*val0 + val1, 3*val1))
+    | otherwise = computeString(s, (val0, val1))
 
 main = do
     line <- getLine
-    putStrLn ( show (computeStar (line, 1)))
+    putStrLn ( show (fst (computeString (line, (1, 1)))))
