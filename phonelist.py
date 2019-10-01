14bcac23bf79dd23d31818e9210bb6cc98d22118
Adrien Coulier <adrien.coulier@it.uu.se>
2019-10-01 18:57:27 +0200

    Submit phonelist
    RE

---
+++ phonelist.py
@@ -0,0 +1,24 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    try:
+        phone_book = {}
+        for j in range(n):
+            number = input()
+            L = len(number) - 1
+            d = phone_book
+            for i, c in enumerate(number):
+                if i < L:
+                    if c in d and d[c] == {}:
+                        raise Exception()
+                    else:
+                        d = d.setdefault(c, {})
+                elif c in d:
+                    raise Exception()
+                else:
+                    d[c] = {}
+    except Exception:
+        print("NO")
+    else:
+        print("YES")
+
