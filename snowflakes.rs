bc81d9f9fdd8f3ba63a0b6740114953c31539380
Adrien Coulier <adrien.coulier@it.uu.se>
2020-04-10 17:28:53 +0200

    Attempt snowflakes
    WA

---
+++ snowflakes.rs
@@ -0,0 +1,28 @@
+use std::io::{self, BufRead};
+use std::cmp::max;
+use std::collections::HashMap;
+
+fn main() {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+    let t = lines.next().unwrap().unwrap().parse::<usize>().unwrap();
+
+    for _ in 0..t {
+        let n = lines.next().unwrap().unwrap().parse::<usize>().unwrap();
+        let mut last_seen = HashMap::new();
+
+        let mut longest_streak = 0;
+        let mut most_recent_dupl = 0;
+
+        for i in 0..n {
+            let snowflake = lines.next().unwrap().unwrap().parse::<usize>().unwrap();
+
+            if let Some(prev) = last_seen.insert(snowflake, i) {
+                most_recent_dupl = max(most_recent_dupl, prev);
+            }
+
+            longest_streak = max(longest_streak, i - most_recent_dupl);
+        }
+        println!("{}", longest_streak);
+    }
+}
