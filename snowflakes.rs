f2b362dba9346f5a33edf0805ea7d5bc8cfbd344
Adrien Coulier <adrien.coulier@it.uu.se>
2020-04-10 17:34:32 +0200

    Solve snowflakes
    AC

--- snowflakes.rs
+++ snowflakes.rs
@@ -5,17 +5,17 @@ use std::collections::HashMap;
 fn main() {
     let stdin = io::stdin();
     let mut lines = stdin.lock().lines();
-    let t = lines.next().unwrap().unwrap().parse::<usize>().unwrap();
+    let t = lines.next().unwrap().unwrap().parse::<i32>().unwrap();
 
     for _ in 0..t {
-        let n = lines.next().unwrap().unwrap().parse::<usize>().unwrap();
+        let n = lines.next().unwrap().unwrap().parse::<i32>().unwrap();
         let mut last_seen = HashMap::new();
 
         let mut longest_streak = 0;
-        let mut most_recent_dupl = 0;
+        let mut most_recent_dupl = -1;
 
         for i in 0..n {
-            let snowflake = lines.next().unwrap().unwrap().parse::<usize>().unwrap();
+            let snowflake = lines.next().unwrap().unwrap().parse::<i32>().unwrap();
 
             if let Some(prev) = last_seen.insert(snowflake, i) {
                 most_recent_dupl = max(most_recent_dupl, prev);
