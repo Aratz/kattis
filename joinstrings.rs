5a0420968b0aae245bac85341c85bdb84b712335
Adrien Coulier <adrien.coulier@it.uu.se>
2020-04-15 16:20:24 +0200

    Attempt joinstrings
    TLE

--- joinstrings.rs
+++ joinstrings.rs
@@ -7,25 +7,39 @@ fn main() {
 
     let n:usize = lines.next().unwrap().unwrap().parse().unwrap();
 
-    let mut strings:Vec<String> = Vec::new();
+    let mut strings:Vec<String> = Vec::with_capacity(n);
+    let mut next: Vec<usize> = Vec::with_capacity(n);
 
-    for _ in 0..n {
+    for i in 0..n {
         strings.push(String::from(lines.next().unwrap().unwrap()));
+        next.push(i);
     }
+
 
     let mut last = 0;
 
     for _ in 0..n - 1 {
         let ab = lines.next().unwrap().unwrap().split(" ")
             .map(|v| v.parse::<usize>().unwrap()).collect::<Vec<_>>();
-        let (a, b) = (ab[0] - 1, ab[1] - 1);
-
-        let new_string = String::from(&strings[a]) + &strings[b];
-
-        strings[a] = new_string;
-        strings[b] = String::new();
+        let (mut a, b) = (ab[0] - 1, ab[1] - 1);
 
         last = a;
+
+        while a != next[a] {
+            a = next[a];
+        }
+        next[a] = b;
     }
-    println!("{}", strings[last]);
+
+    loop {
+        print!("{}", strings[last]);
+
+        if last == next[last] {
+            println!();
+            break;
+        }
+        else {
+            last = next[last];
+        }
+    }
 }
