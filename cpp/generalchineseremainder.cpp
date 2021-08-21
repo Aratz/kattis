11e2febae1ba72449defee9932e18e6e51a57aff
Adrien Coulier <adrien.coulier@it.uu.se>
2021-08-21 21:35:17 +0200

    Attempt generalchineseremainder
    WA

---
+++ cpp/generalchineseremainder.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <utility>
+
+using namespace std;
+
+typedef pair<long long, long long> ll;
+typedef pair<long long, ll> lll;
+
+lll xgcd(long long a, long long b) {
+    long long old_r = a; long long r = b;
+    
+    long long old_s = 1; long long s = 0;
+    long long old_t = 0; long long t = 1;
+
+    while(r != 0) {
+        long long quotient = old_r / r;
+
+        long long temp_r = old_r;
+        old_r = r;
+        r = temp_r - quotient*r;
+
+        long long temp_s = old_s;
+        old_s = s;
+        s = temp_s - quotient*s;
+
+        long long temp_t = old_t;
+        old_t = t;
+        t = temp_t - quotient*t;
+    }
+
+    return make_pair(old_r, make_pair(old_s, old_t));
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while(T--) {
+        long long a, n, b, m;
+        cin >> a >> n >> b >> m;
+
+        lll uv = xgcd(n, m);
+        long long g = uv.first;
+        if(a % g == b % g) {
+            long long u = uv.second.first; long long v = uv.second.second;
+
+            //cerr << u*n << " " << v*m << " " << g << "\n";
+
+            long long K = m*(n/g);
+
+            long long x = (a*(v*(m/g) % K) % K) + (b*(u*(n/g) % K) % K);
+            cout << (x + K)%K << " " << K << "\n";
+        }
+        else {
+            cout << "no solution\n";
+        }
+    }
+    return 0;
+}
