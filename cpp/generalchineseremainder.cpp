7bf4b0476888a48c5cbf3ee316113d39c7262f2a
Adrien Coulier <adrien.coulier@it.uu.se>
2021-08-23 17:48:08 +0200

    Solve generalchineseremainder
    AC

--- cpp/generalchineseremainder.cpp
+++ cpp/generalchineseremainder.cpp
@@ -6,9 +6,31 @@ using namespace std;
 typedef pair<long long, long long> ll;
 typedef pair<long long, ll> lll;
 
+long long mulmod(long long a, long long b, long long mod) {
+    long long res = 0;
+    a = a % mod;
+
+    if(b < 0) {
+        a = -a;
+        b = -b;
+    }
+
+    while(b > 0) {
+        if (b % 2 == 1) {
+            res += a;
+            res %= mod;
+        }
+
+        a *= 2; a %= mod;
+
+        b /= 2;
+    }
+
+    return res % mod;
+}
+
 lll xgcd(long long a, long long b) {
     long long old_r = a; long long r = b;
-    
     long long old_s = 1; long long s = 0;
     long long old_t = 0; long long t = 1;
 
@@ -43,11 +65,11 @@ int main() {
         if(a % g == b % g) {
             long long u = uv.second.first; long long v = uv.second.second;
 
-            //cerr << u*n << " " << v*m << " " << g << "\n";
-
             long long K = m*(n/g);
 
-            long long x = (a*(v*(m/g) % K) % K) + (b*(u*(n/g) % K) % K);
+            //long long x = ((a*(v*(m/g) % K) % K) + (b*(u*(n/g) % K) % K)) % K;
+            long long x = mulmod(a, mulmod(v, m/g, K), K) + mulmod(b, mulmod(u, n/g, K), K);
+            x %= K;
             cout << (x + K)%K << " " << K << "\n";
         }
         else {
